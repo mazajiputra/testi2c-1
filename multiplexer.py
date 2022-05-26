b8c4bc64726b394435a9e819756a310b9faf95fc
mazajiputra <mazajiputra@gmail.com>
2022-05-26 13:21:01 +0700

    Update multiplexer.py

--- multiplexer.py
+++ multiplexer.py
@@ -8,7 +8,7 @@ import time
 import sys
 
 I2C_address = 0x77
-I2C_bus_number = 1
+I2C_bus_number = 3
 I2C_ch_0 = 0b00000001
 I2C_ch_1 = 0b00000010
 I2C_ch_2 = 0b00000100
@@ -22,6 +22,6 @@ def I2C_setup(i2c_channel_setup):
     bus = smbus.SMBus(I2C_bus_number)
     bus.write_byte(I2C_address,i2c_channel_setup)
     time.sleep(0.1)
-    print "TCA9548A I2C channel status:", bin(bus.read_byte(I2C_address))
+    print ("TCA9548A I2C channel status:", bin(bus.read_byte(I2C_address)))
 
 I2C_setup(int(sys.argv[1]))
